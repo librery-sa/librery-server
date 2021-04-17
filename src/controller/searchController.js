8d8b136f79b8e38868b57ae994f9d6ff1e4b769f
Odilon Damasceno <storckskey@gmail.com>
2021-04-17 19:35:33 -0300

    feat: create searchController

--- src/controller/searchController.js
+++ src/controller/searchController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ObjectID } = require('mongodb');
 const User = require("../models/User");
+const authMiddleware = require('../middlewares/auth');
 const Community = require('../models/Community');
 const router = express.Router();
 
@@ -21,6 +22,24 @@ router.get('/community/:id', async (req, res) => {
         res.send(community);
     } catch (e) {
         res.status(404).send({ error: "Failed to find community" });
+    }
+});
+
+router.use(authMiddleware);
+
+router.get('/user/feed', async (req, res) => {
+    try {
+        const user = await User.findById(req.userId, { _id: false }).select('following');
+        let feed = [];
+        user.following.forEach(async comm => {
+            feed.push(await Community.find({ idend: comm }, { _id: false }).select("sources"));
+        });
+        feed.sort((a, b) => {
+            return b.createdAt - a.createdAt;
+        });
+        res.send({ feed });
+    } catch (e) {
+        res.status(400).send({ error: e });
     }
 });
 
